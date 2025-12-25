ab2bf568f8edb6d8d6b357b77245286d586525bf
nkpossed818 <nkposssed@gmail.com>
2025-12-25 20:41:06 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const membersData = {
     'terror': { name: 'Terror', role: '#drc', img: '', desc: 'X', link: 'X' },
-    'vianna': { name: 'Vianna', role: '#drc', img: 'https://i.postimg.cc/Kvmc5GK8/IMG-20251225-195322-595.jpg', desc: 'Lealdade ao rei', link: 'https://atoinfracional' },
+    'vianna': { name: 'Vianna', role: '#drc', img: 'https://i.postimg.cc/Kvmc5GK8/IMG-20251225-195322-595.jpg', desc: 'Lealdade ao rei', link: 'https://t.me/atoinfracional' },
     'nk': { name: 'nk', role: '#drc', img: 'https://i.postimg.cc/QNnyxSpS/IMG-20251225-192923-071.jpg', desc: 'Jesus is king', link: 'https://t.me/santificam' },
     'richard': { name: 'Richard', role: '#drc', img: 'https://i.postimg.cc/ZKw7DHqN/IMG-20251225-200406-605.jpg', desc: 'Se vires seu amigo se associando com seu inimigo, saiba que você tem dois inimigos: um declarado e um secreto.', link: 'https://t.me/diorczar' },
     'sod3ad': { name: 'Sod3ad', role: '#drc', img: 'https://i.postimg.cc/JhD2XXMt/IMG-20251225-200114-905.jpg', desc: 'Demônios que jesus mandou pros porcos, retornou lobo em pele de cordeiro.', link: 'https://t.me/duslog' },
