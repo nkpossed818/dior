a8c8ea05b073fddc44ac16a8c206019399469e94
nkpossed818 <nkposssed@gmail.com>
2026-02-18 17:00:37 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const membersData = {
     'vitin': { name: 'Vitin', role: '#drc', img: 'https://i.postimg.cc/JhXWLvy6/IMG-20251225-202254-685.jpg', desc: 'ㅤ', link: 'https://t.me/torturava' },
     'cobra': { name: 'Cobra', role: '#drc', img: 'https://i.postimg.cc/g2rLH0wd/IMG-20260218-155524-749.jpg', desc: 'يا إلهي جون', link: 'https://t.me/omegacarbonn' },
     'Skype': { name: 'Skype', role: '#drc', img: 'https://i.postimg.cc/28gcD64W/IMG-20251225-203028-263.jpg', desc: '.', link: 'https://t.me/diorczar' },
-    'wlrd': { name: 'Wlrd', role: '#drc', img: 'https://i.postimg.cc/hPQspBN5/IMG-20251225-203155-756.jpg', desc: 'osint, csint & forensics nerd', link: 'https://t.me/lover5224047' },
+    'wlrd': { name: 'Wlrd', role: '#drc', img: 'https://i.postimg.cc/K8CZdPNM/IMG-20260218-165509-448.jpg', desc: 'osint, csint & forensics nerd', link: 'https://t.me/lover5224047' },
     'arthur': { name: 'Arthur', role: '#drc', img: 'https://i.postimg.cc/FH55RVMN/IMG-20251225-211845-873.jpg', desc: 'O caos é a matriz da criação.', link: 'https://t.me/ameacar'},
     'bey': { name: 'Bey', role: '#drc', img: 'https://i.postimg.cc/hPQspBN5/IMG-20251225-203155-756.jpg', desc: 'osint', link: 'https://t.me/pcrsc'},
     'meliodas': { name: 'Meliodas', role: '#drc', img: 'https://i.postimg.cc/FH55RVMN/IMG-20251225-211845-873.jpg', desc: 'Seu pecado; meu julgamento.', link: 'https://t.me/sacrificacao'},
